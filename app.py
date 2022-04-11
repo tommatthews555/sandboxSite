c532e53692ccfd8fe02df5ef5cae8ec2227743fa
Tom Matthews <tom.matthews@bluestaq.com>
2022-04-11 10:36:05 -0600

    lotsa files, sqlalchemy

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html")
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Week(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    deliveryDate = db.Column(db.DateTime(), nullable=False)
+
+    def __repr__(self):
+        return '<Week %r>' % self.deliveryDate
+
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    desc = db.Column(db.String(), nullable=False)
+    gf = db.Column(db.Boolean(), nullable=False, default=False)
+    df = db.Column(db.Boolean(),  nullable=False, default=False)
+    vgt = db.Column(db.Boolean(),  nullable=False, default=False)
+    vgn = db.Column(db.Boolean(),  nullable=False, default=False)
+    
---
+++ env.bat
@@ -0,0 +1,3 @@
+$env:FLASK_APP="app.py"
+$env:API_KEY="pk_6fa487b7676f46548fc629a7134bfde5"
+py -3 -m venv venv
